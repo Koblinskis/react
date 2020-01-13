81b49acc18ce7e0218dabdf4c22da54941e85041
Micheal Koblinski <stevenkoblinskip@gmail.com>
2020-01-13 15:35:16 -0500

    setting up react redux and using actions

--- src/app.js
+++ src/app.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore';
+import { addExpense, removeExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore()
+
+
+store.subscribe(() => {
+  const state = store.getState()
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+  console.log(visibleExpenses);
+});
+
+store.dispatch(addExpense({ description:'bill', amount: 600 }))
+store.dispatch(addExpense({ description:'gas bill', amount: 400, createdAt: 1000}))
+store.dispatch(addExpense({ description:'water bill', amount: 1000 }))
+
+const jsx = (
+  <Provider store={store} >
+    <AppRouter />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
